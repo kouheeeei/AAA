5c184442e4993f45013ebc16f26127b1c86f7cc8
A <A>
2023-08-18 19:24:19 +0900

    4th commit

--- templates/map.js
+++
@@ -1,7 +0,0 @@
-function initMap() {
-  var opts = {
-      zoom: 15,
-      center: new google.maps.LatLng(35.690921, 139.700258)
-  };
-var map = new google.maps.Map(document.getElementById("map"), opts);
-}
